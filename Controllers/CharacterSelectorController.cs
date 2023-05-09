ecb03b826f4a1a2d16f7266249bd6cc518cb82b6
Krictic <pedrotorresgamedesign@gmail.com>
2023-05-09 01:05:26 -0300

    Removed recursive method call to correct a possible StackOverflow error if the uses inputs too many invalid options.

--- Controllers/CharacterSelectorController.cs
+++ Controllers/CharacterSelectorController.cs
@@ -11,26 +11,28 @@ namespace ShaRPGame.Controllers
         // Todo: make
         public static void ListProcessing()
         {
+            bool validInput = false;
             int input = 0;
             CharacterListModel CharaList = CharacterSelectorModel.ListAll();
             Console.WriteLine("Please, select the index of the character you wish to select to activate it.");
-            
-            try
+            while (!validInput)
             {
-                input = Convert.ToInt32(Gui.GetInputIntBasic(input).Trim());
-                CharacterModel character = CharaList.GetCharacterList()[input];
-                CharacterSelectorModel.ActivateCharacter(character);
-                ActiveCharacterModel activeCharacterInstance = ActiveCharacterModel.GetIntance();
-                CharacterModel activeCharacter = activeCharacterInstance.GetActiveCharacter();
-                Gui.PrintLine(activeCharacter.GetName() + " has been activated.");
-                MainMenuView.Menu();
+                try
+                {
+                    input = Convert.ToInt32(Gui.GetInputIntBasic(input));
+                    CharacterModel character = CharaList.GetCharacterList()[input];
+                    CharacterSelectorModel.ActivateCharacter(character);
+                    ActiveCharacterModel activeCharacterInstance = ActiveCharacterModel.GetIntance();
+                    CharacterModel activeCharacter = activeCharacterInstance.GetActiveCharacter();
+                    Gui.PrintLine(activeCharacter.GetName() + " has been activated.");
+                    MainMenuView.Menu();
+                    validInput = true;
+                }
+                catch (Exception ex)
+                {
+                    Gui.Alert(ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
-                Gui.Alert(ex.Message);
-                ListProcessing();
-            }
-            
         }
     }
 }
